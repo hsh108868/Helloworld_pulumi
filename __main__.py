ea0ad3317c72a48e13447031c0a5fc1de77ea7d5
hsh108868 <pusrty@gmail.com>
2022-10-16 22:33:35 +0900

    Modify main.py

--- __main__.py
+++ __main__.py
@@ -1,10 +1,4 @@
 """An AWS Python Pulumi program"""
 import pulumi
-import pulumi_aws as ec2
+import pulumi_kubernetes as k8s
 
-
-size = 't2.micro'
-
-if ec2.get_ami == 0:
-    ami = ec2.get_ami(most_recent=True,
-        owners=[""])
