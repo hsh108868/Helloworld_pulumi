eb27551b66569132868f891b9bc1a5c5b0a31105
hsh108868 <pusrty@gmail.com>
2022-10-26 22:20:22 +0900

    Modify todo.swift

--- todo/todo.swift
+++ todo/todo.swift
@@ -3,7 +3,7 @@ class ViewController: UIViewController {
     @IBOutlet var editButton: UIBarButtonItem!
     var doneButton: UIBarButtonItem?
     var tasks = [Task]()  {
-        didSet { // 프로퍼티 옵저버, tasks 배열에 할일이 추가될 때마다 유저 디폴트에 할일이 저장됨
+        didSet { 
             self.saveTasks()
         }
     }// Task 배열 생성
