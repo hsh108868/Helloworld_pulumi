b48114fad8e365b8d358ce629dd9d640f55bab60
hsh108868 <pusrty@gmail.com>
2022-10-28 15:20:40 +0900

    Modify main.py

--- __main__.py
+++ __main__.py
@@ -1,8 +1,11 @@
 """An AWS Python Pulumi program"""
 import pulumi
 import pulumi_kubernetes as k8s
+import pulumi_docker as docker
 import pulumi_eks as eks
 
+
+stack = pulumi.get_stack()
 
 cluster = eks.Cluster('app-cluster')
 
--- todo/todo.swift
+++
@@ -1,9 +0,0 @@
-class ViewController: UIViewController {
-    @IBOutlet weak var tableView: UITableView!
-    @IBOutlet var editButton: UIBarButtonItem!
-    var doneButton: UIBarButtonItem?
-    var tasks = [Task]()  {
-        didSet { 
-            self.saveTasks()
-        }
-    }
