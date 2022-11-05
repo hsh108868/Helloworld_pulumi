8d4c9369089f7c47c42642e2ec38d66208d86fa6
hsh108868 <pusrty@gmail.com>
2022-11-05 21:47:05 +0900

    Modify main.py

--- __main__.py
+++ __main__.py
@@ -7,7 +7,9 @@ import pulumi_eks as eks
 
 stack = pulumi.get_stack()
 
-repo = aws.ecr.Repository("myrepo", image_scanning_configuration=True)
+repo = aws.ecr.Repository(
+    "myrepo", image_scanning_configuration=True, image_tag_mutability="MUTABLE")
+
 
 cluster = eks.Cluster('app-cluster')
 
