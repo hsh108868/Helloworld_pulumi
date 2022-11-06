c8fdb7c15ef2d4fd3fac7bddc9662645878ae12a
hsh108868 <pusrty@gmail.com>
2022-11-06 22:40:30 +0900

    Modify main.py

--- __main__.py
+++ __main__.py
@@ -10,7 +10,6 @@ stack = pulumi.get_stack()
 repo = aws.ecr.Repository(
     "myrepo", image_scanning_configuration=True, image_tag_mutability="MUTABLE")
 
-
 cluster = eks.Cluster('app-cluster')
 
 pulumi.export('kubeconfig', cluster.kubeconfig)
