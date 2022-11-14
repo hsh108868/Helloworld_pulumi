9876b3f537e3673a79820de4f9d386ddc8715e42
hsh108868 <pusrty@gmail.com>
2022-11-14 22:48:37 +0900

    Modify main.py

--- __main__.py
+++ __main__.py
@@ -4,9 +4,6 @@ import pulumi_kubernetes as k8s
 import pulumi_aws as aws
 import pulumi_eks as eks
 
-
-stack = pulumi.get_stack()
-
 repo = aws.ecr.Repository(
     "myrepo", image_scanning_configuration=True, image_tag_mutability="MUTABLE")
 
