6b632da3660990bae021131469c53073f012f302
hsh108868 <pusrty@gmail.com>
2022-11-16 00:00:16 +0900

    Modify main.py

--- __main__.py
+++ __main__.py
@@ -4,9 +4,10 @@ import pulumi_kubernetes as k8s
 import pulumi_awsx as aws
 import pulumi_eks as eks
 
-repo = aws.ecr.Repository(
-    "myrepo", image_scanning_configuration=True, image_tag_mutability="MUTABLE")
 
-customImage = "hello_world"
-appImage = repo.buildAndPushImage('./hello_world/${customImage}')
+repo = aws.ecr.Repository("my_repo")
 
+image = aws.ecr.Image(
+    "hello_world", repository_url=repo.url, path="./hello_world")
+
+provider = k8s.Provider("hello_world", config.kubeconfig)
