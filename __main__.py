eac7085d21388e46cd9574e419fa58394c6a16e4
hsh108868 <pusrty@gmail.com>
2022-11-18 04:36:44 +0900

    Modify main.py

--- __main__.py
+++ __main__.py
@@ -8,6 +8,20 @@ import pulumi_eks as eks
 repo = aws.ecr.Repository("my_repo")
 
 image = aws.ecr.Image(
-    "hello_world", repository_url=repo.url, path="./hello_world")
+    resource_name="hello_world", repository_url=repo.url, path="./hello_world")
 
-provider = k8s.Provider("hello_world", kubeconfig=config, )
+provider = k8s.Provider(resource_name="hello_world", namespace="hello_world")
+
+appDeployment = k8s.apps.v1.Deployment("apps", spec=k8s.apps.v1.DeploymentSpecArgs(
+    metadata=k8s.meta.v1.ObjectMetaArgs(
+        labels={
+            "app": "nginx",
+        },
+    ),
+    replicas=1,
+    selector=k8s.meta.v1.LabelSelectorArgs(
+        match_labels={
+            "app": "nginx"
+        }
+    )
+))
