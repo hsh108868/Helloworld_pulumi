96119a9f8f5b941cc1ee4fb850035fdaf15f95fc
hsh108868 <pusrty@gmail.com>
2022-11-19 02:38:35 +0900

    Modify main.py

--- __main__.py
+++ __main__.py
@@ -7,21 +7,39 @@ import pulumi_eks as eks
 
 repo = aws.ecr.Repository("my_repo")
 
-image = aws.ecr.Image(
-    resource_name="hello_world", repository_url=repo.url, path="./hello_world")
+appImage = "hello_world"
+customImage = aws.ecr.Image(
+    resource_name=appImage, repository_url=repo.url, path="./hello_world")
 
-provider = k8s.Provider(resource_name="hello_world", namespace="hello_world")
+provider = k8s.Provider(resource_name=appImage, namespace=appImage)
 
 appDeployment = k8s.apps.v1.Deployment("apps", spec=k8s.apps.v1.DeploymentSpecArgs(
     metadata=k8s.meta.v1.ObjectMetaArgs(
         labels={
-            "app": "nginx",
+            "app": appImage,
         },
     ),
     replicas=1,
     selector=k8s.meta.v1.LabelSelectorArgs(
         match_labels={
-            "app": "nginx"
+            "app": appImage,
         }
+    ),
+    template=k8s.core.v1.PodTemplateSpecArgs(
+        metadata=k8s.meta.v1.ObjectMetaArgs(
+            labels={
+                "app": appImage,
+            }
+        ),
+        spec=k8s.core.v1.PodSpecArgs(
+            containers=k8s.core.v1.ContainerArgs(
+                name=appImage,
+                image=customImage,
+                ports=k8s.core.v1.ContainerPortArgs(
+                    name="http",
+                    container_port=80,
+                )
+            )
+        )
     )
-))
+),)
